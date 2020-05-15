ec0581cbd85ac9efd2ab3fa862d2d83c505fa0e3
Biljana <zabiljanupavlovic@gmail.com>
2020-05-15 16:48:23 +0200

    comments fixed

--- src/review/coment.jsx
+++ src/review/coment.jsx
@@ -1,13 +1,9 @@
 import React, { useState, useContext } from "react";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import { CommentList, CommentContext } from "./commentList";
-import singleComment from "./singleComment";
+
+import { CommentContext } from "./commentList";
+import SingleComment from "./singleComment";
 
 export default function UsersComment() {
-  const [counter, setCounter] = useState(0);
-  const [negativCounter, setNegativCounter] = useState(0);
   const [name, setName] = useState("");
   const [comment, setComment] = useState("");
   const [comments, commentList] = useContext(CommentContext);
@@ -22,7 +18,8 @@ export default function UsersComment() {
 
   const addComment = (e) => {
     e.preventDefault();
-    CommentList((prevComments) => [
+    commentList((prevComments) => [
+      ...prevComments,
       {
         name: name,
         comment: comment,
@@ -30,47 +27,21 @@ export default function UsersComment() {
     ]);
   };
 
-  const increase = () => {
-    setCounter(counter + 1);
-  };
-  const decrease = () => {
-    setNegativCounter(negativCounter - 1);
-  };
-  console.log(CommentList)
   return (
     <div className="comment">
-      <p>Napišite nam svoj komentar ako ste posetili ovu birtiju</p>
-      {comments.map(comment => (
-        <CommentList
-          name={name}
-          comments={comment}
-          
+      <p>Komentari posetilaca sajta:</p>
+      {comments.map((comment) => (
+        <SingleComment
+          name={comment.name}
+          comment={comment.comment}
+          key={comment.id}
         />
       ))}
-
-      <Container>
-        <Row>
-          <Col>
-            
-            <h3>{counter}</h3>
-            <button className="btn-like" onClick={increase}>
-              <img className="btn-like" src="/img/like.png" alt="krigla" />
-            </button>
-          </Col>
-          <Col>
-            <h3>{negativCounter}</h3>
-            <button className="btn-like" onClick={decrease}>
-              <img src="/img/dislajk.png" className="btn-like" alt="krigla" />
-            </button>
-          </Col>
-        </Row>
-      </Container>
-
-
-      <form onSubmit={addComment}>
+<p>Napišite nam svoj komentar ako ste posetili ovu birtiju</p>
+      <form>
         <input
           type="text"
-          className = 'input-comment'
+          className="input-comment"
           placeholder="Vase ime"
           name="name"
           value={name}
@@ -79,13 +50,16 @@ export default function UsersComment() {
         <br />
         <input
           type="text"
-          className = 'input-comment'
+          className="input-comment"
           name="comment"
           value={comment}
           onChange={uptadeComment}
           placeholder="Komentar"
-        /><br/>
-        <button className="btn-about">Posalji</button>
+        />
+        <br />
+        <button className="btn-about" onClick={addComment}>
+          Posalji
+        </button>
       </form>
     </div>
   );
--- src/review/singleComment.jsx
+++ src/review/singleComment.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 
-export default function singleComment({ name, comment }) {
+
+export default function SingleComment({ name, comment }) {
+  const [counter, setCounter] = useState(0);
+  const [negativCounter, setNegativCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(counter + 1);
+  };
+  const decrease = () => {
+    setNegativCounter(negativCounter - 1);
+  };
   return (
-    <div>
-      <h6>{name}</h6>
-      <p>{comment}</p>
+    <div className = 'single-comment'>
+      <h6>Ime: {name}</h6>
+      <p>Komentar: {comment}</p>
+      
+      <Container>
+        <Row>
+          <Col>
+            
+            <h3>{counter}</h3>
+            <button className="btn-like" onClick={increase}>
+              <img className="btn-like" src="/img/like.png" alt="krigla" />
+            </button>
+          </Col>
+          <Col>
+            <h3>{negativCounter}</h3>
+            <button className="btn-like" onClick={decrease}>
+              <img src="/img/dislajk.png" className="btn-like" alt="krigla" />
+            </button>
+          </Col>
+        </Row>
+     </Container>
     </div>
   );
 }
